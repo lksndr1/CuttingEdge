aee2c23f204bc9f024a0d73fb165d871731a9830
hettfield git <olekpl90@gmail.com>
2025-01-09 11:10:38 +0100

    learn-more section, data

--- src/components/LearnMoreSection.jsx
+++ src/components/LearnMoreSection.jsx
@@ -1,7 +1,18 @@
+import ImportantFeatures from "./ImportantFeatures"
+import { features } from '../data'
+
 export default function LearnMoreSection() {
     return (
         <section>
-            <h3>Learn more section</h3>
-        </section>
+            <h3>Why cutting parameters in machinning are important</h3>
+
+            <ul>
+                {features.map((feature) => {
+                    return (
+                        <ImportantFeatures key={feature.title} {...feature} />
+                    )
+                })}
+            </ul>
+        </section >
     )
 }
